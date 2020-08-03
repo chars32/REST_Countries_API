20dd9c7677ef960b5264df7a3fa868951298a651
Carlos Garcia Castellanos <chars24@gmail.com>
2020-08-03 17:54:45 -0500

    1.2 API cuando carga pantalla inicial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Header from "./components/header";
 import Searchfilter from "./components/searchFilter";
 import Flags from "./components/flags";
 
+import { AllFlags } from "./scripts/data";
+
 const App = () => {
   const [dark, setDark] = useState(false);
+  const [flagsInfo, setFlagsInfo] = useState();
 
   const handlerClick = () => {
     setDark(!dark);
   };
 
+  const allFLgagsData = async () => {
+    const data = await AllFlags();
+    setFlagsInfo(data);
+  };
+
+  useEffect(() => {
+    allFLgagsData();
+  }, []);
+
   return (
     <div className={`App ${dark ? "darkApp" : "lightApp"}`}>
       <Header mode={handlerClick} dark={dark} />
       <Searchfilter dark={dark} />
-      <Flags dark={dark} />
+      <Flags dark={dark} flagsData={flagsInfo} />
     </div>
   );
 };
---
+++ src/scripts/data.js
@@ -0,0 +1,9 @@
+const AllFlags = async () => {
+  const api_call = await fetch("https://restcountries.eu/rest/v2/all");
+  const data = await api_call.json();
+  return data;
+};
+
+module.exports = {
+  AllFlags,
+};
